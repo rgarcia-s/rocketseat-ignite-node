7f3d4683c423316f012a4d503d0f0b27e7ac6793
Raul Garcia <raul.gsouza@gmail.com>
2021-04-12 20:53:08 -0300

    Initial commit

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,13 @@
+import { Specification } from '../entities/Specification';
+
+interface ICreateSpecificationsDTO {
+  name: string;
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ name, description }: ICreateSpecificationsDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationsDTO };
